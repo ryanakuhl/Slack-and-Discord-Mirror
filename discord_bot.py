55f2294d5953fd56ce8c3ce35790cca72ae9b8fd
RKuhl <ryanakuhl@gmail.com>
2019-11-18 15:44:57 -0500

    Update discord_bot.py

--- discord_bot.py
+++ discord_bot.py
@@ -5,11 +5,12 @@ cur = con.cursor()
 img_sql = "INSERT INTO obdm_img (hyperlink, name) VALUES (?, ?)"
 
 discord_bot_token = 'Found at https://discordapp.com/developers/applications/'
+discord_channel = 'Found by looking at the Discord Channel URL'
 client = discord.Client()
 
 @client.event
 async def on_ready():
-    channel = client.get_channel(646000041567453184)
+    channel = client.get_channel(discord_channel)
     messages = await channel.history().flatten()
     for message in messages:
         try:
